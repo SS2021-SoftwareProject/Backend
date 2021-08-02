e529b9a41f49d13f7c73cd290ca2e9153c4a3b2c
Saryna | さりな <45205532+Saryna42@users.noreply.github.com>
2021-08-01 21:29:16 -0700

    Milestone sample data

--- api/db/sampleData.py
+++ api/db/sampleData.py
@@ -1,11 +1,35 @@
-import requests
+# This file is not a working function or executable data, just the structure for some random values.
 
-url = "http://127.0.0.1:5000/api/milestone"
-obj = {
-    "name":"testMeilenstein",
-    "idImage":"5",
-    "amount":"1200.0",
-    "description":"Testbeschreibung"
-}
-x = requests.post(url,headers = obj)
-print(x.text)
+
+def add_sample_data():
+    """
+    Adds some sample data.
+    Again, not a working function atm.
+    :return: -
+    """
+
+    #fictional milestones, sample data
+
+    milestones: List[Milestone] = [
+        Milestone(idMilestone=1,
+            nameMilestone="Organisation and roles",
+            amountMilestone=12,
+            descriptionMilestone="Decide who does what and who controls and leads the teams"),
+        Milestone(idMilestone=2,
+            nameMilestone="Modeling",
+            amountMilestone=34,
+            descriptionMilestone="Model of the database structure"),
+        Milestone(idMilestone=3,
+            nameMilestone="Database",
+            amountMilestone=45,
+            descriptionMilestone="Creation of the database"),
+        Milestone(idMilestone=4,
+            nameMilestone="API",
+            amountMilestone=67,
+            descriptionMilestone="Creation of the API for easy access to the database"),
+        Milestone(idMilestone=5,
+            nameMilestone="GUI",
+            amountMilestone=89,
+            descriptionMilestone="Visually pleasing GUI to provide access to the API instead of commands"),
+    ]
+
